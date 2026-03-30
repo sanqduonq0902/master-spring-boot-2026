12e598691f969e4fe818d87fc53fdd6f8f588be5
SanqDuonq <namsang0902s@gmail.com>
2026-03-30 13:38:21 +0700

    init project

---
+++ src/main/java/com/project/masterspringboot2026/MasterSpringBoot2026Application.java
@@ -0,0 +1,13 @@
+package com.project.masterspringboot2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringBoot2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringBoot2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/masterspringboot2026/MasterSpringBoot2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.masterspringboot2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringBoot2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
