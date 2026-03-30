9f09d36ccdcbede1d966b410b2a20bd3290f49aa
SanqDuonq <namsang0902s@gmail.com>
2026-03-30 22:16:16 +0700

    feat: CRUD users

---
+++ src/main/java/com/project/masterspringboot2026/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.project.masterspringboot2026.repository;
+
+import com.project.masterspringboot2026.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
